895b19f090ed884e86df5d402fbee207235e743a
Romildo Jr <fis.romildojr@gmail.com>
2023-10-10 21:58:17 -0300

    Bakup

--- src/main/java/com/example/pw3/Pw3Application.java
+++ src/main/java/com/example/pw3/Pw3Application.java
@@ -2,8 +2,17 @@ package com.example.pw3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+// @ComponentScan(basePackages = {
+// 	"com.example.pw3.controllers",
+// 	"com.example.pw3.models",
+// 	"com.example.pw3.repositories",
+// 	"com.example.pw3.security",
+// 	"com.example.pw3.services",
+
+// })
 public class Pw3Application {
 
 	public static void main(String[] args) {
